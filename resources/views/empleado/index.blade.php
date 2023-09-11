25998295e6beb4cc595280ecee74245dbfe2491f
Victor Mendoza <willradke1@gmail.com>
2023-09-11 14:12:23 -0600

    mostras lista

--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -1,37 +1,53 @@
-Mostrar lista empleados
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Mostrar lista empleados
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <table class="table table-light">
+                    
+                        <thead class="thead-light">
+                            <tr>
+                                <th>#</th>
+                                <th>Foto</th>
+                                <th>Nombre </th>
+                                <th>Apellido Paterno </th>
+                                <th>Apellido Materno </th>
+                                <th>Numero Tel </th>
+
+                                <th>Acciones </th>
+
+                            </tr>
+                        </thead>
+                        
+                        <tbody>
+                            @foreach ( $empleados as $empleado ):
+                                <tr>
+                                    <td>{{ $empleado ->id }}</td>
+                                    <td>{{ $empleado ->foto }}</td>
+                                    <td>{{ $empleado ->Nombre }}</td>
+                                    <td>{{ $empleado ->ApellidoPaterno }}</td>
+                                    <td>{{ $empleado ->ApellidoMaterno }}</td>
+                                    <td>{{ $empleado ->Numero_tel }}</td>
+                                    
+                                    <td>  Editar | Borrar </td>
+                                </tr>
+                            @endforeach
+
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
 
 
-<table class="table table-light">
-   
-    <thead class="thead-light">
-        <tr>
-            <th>#</th>
-            <th>Foto</th>
-            <th>Nombre </th>
-            <th>Apellido Paterno </th>
-            <th>Apellido Materno </th>
-            <th>Numero Tel </th>
 
-            <th>Acciones </th>
-
-        </tr>
-    </thead>
-    
-    <tbody>
-        @foreach ( $empleados as $empleado ):
-            <tr>
-                <td>{{ $empleado ->id }}</td>
-                <td>{{ $empleado ->foto }}</td>
-                <td>{{ $empleado ->Nombre }}</td>
-                <td>{{ $empleado ->ApellidoPaterno }}</td>
-                <td>{{ $empleado ->ApellidoMaterno }}</td>
-                <td>{{ $empleado ->Numero_tel }}</td>
-                
-                <td>  Editar | Borrar </td>
-            </tr>
-        @endforeach
-
-    </tbody>
-
-</table>
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmpleadoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/empleados', [EmpleadoController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
