3ed43c2d767793f1dc072bbeacd66842f283b3ae
Victor Mendoza <willradke1@gmail.com>
2023-09-18 13:13:34 -0600

    Botones

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,14 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
+        <div style="display: flex; justify-content: center;">
+        <button style="border: 2px solid black;">Botón 1</button>
+        <button style="border: 2px solid black;">Botón 2</button>
+        <button style="border: 2px solid black;">Botón 3</button>
+        <button style="border: 2px solid black;">Botón 4</button>
+        <button style="border: 2px solid black;">Botón 5</button>
+
+    </div>
     </x-slot>
 
     <div class="py-12">
@@ -14,4 +22,6 @@
             </div>
         </div>
     </div>
+  
+
 </x-app-layout>
