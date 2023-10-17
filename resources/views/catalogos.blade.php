8d5541a5304b1ba56832f6ef368505b2551f27cf
Victor Mendoza <willradke1@gmail.com>
2023-10-17 14:37:52 -0600

    "minilogo"

--- resources/views/catalogos.blade.php
+++ resources/views/catalogos.blade.php
@@ -1,1 +1,70 @@
-aqui van los catalogos
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Mi Página Web</title>
+  <style>
+    /* Estilos del sidebar */
+    .sidebar {
+      width: 250px;
+      background-color: #332;
+      color: #fff;
+      padding: 20px;
+      position: fixed;
+      height: 100%;
+    }
+    
+    .sidebar a {
+      color: #fff;
+      text-decoration: none;
+      display: block;
+      margin-bottom: 20px;
+    }
+
+    /* Estilos de la barra de navegación */
+    .navbar {
+      background-color: #444;
+      color: #fff;
+      padding: 40px;
+    }
+
+    .navbar a {
+      color: #fff;
+      text-decoration: none;
+      margin-right: 80px;
+    }
+  </style>
+</head>
+<body>
+  <!-- Sidebar -->
+  
+  <div class="sidebar">
+    <h2>Sidebar</h2>
+    <a href="#">Enlace 1</a>
+    <a href="#">Enlace 2</a>
+    <a href="#">Enlace 3</a>
+    <a href="#">Enlace 4</a>
+    <a href="#">Enlace 5</a>
+    <a href="#">Enlace 6</a>
+    <a href="#">Enlace 7</a>
+    <a href="#">Enlace 8</a>
+  </div>
+
+  <!-- Contenido principal -->
+  <div style="margin-left: 300px; padding: 0px;">
+    <!-- Barra de navegación -->
+    <div class="navbar">
+      <a href="#">Inicio</a>
+      <a href="#">Acerca de</a>
+      <a href="#">Servicios</a>
+      <a href="#">Contacto</a>
+      <a href="#">Usuario</a> <!-- Enlace para el usuario -->
+    </div>
+    
+    <!-- Contenido de la página -->
+    <h1>Bienvenido a mi página web</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...</p>
+  </div>
+</body>
+</html>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,5 @@
-<!DOCTYPE html>
-
-
-<html lang="en">
-  <head>
+<x-app-layout>
+    <head>
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -17,210 +14,25 @@
     <link rel="stylesheet" href="/assets/css/style.css" />
 
   </head>
-  <body>
-    <div class="container-scroller">
-      <nav class="sidebar sidebar-offcanvas" id="sidebar">
-        <div class="text-center sidebar-brand-wrapper d-flex align-items-center">
-          <a class="sidebar-brand brand-logo" href="index.html"><img src="assets/images/logo.png" alt="logo" /></a>
-
-        </div>
-        <ul class="nav">
-          <li class="nav-item nav-profile">
-            <a href="#" class="nav-link">
-              <div class="nav-profile-image">
-              
-                <span class="login-status online"></span>
-                <!--change to offline or busy as needed-->
-              </div>
-              <div class="nav-profile-text d-flex flex-column pr-3">
-              
-              </div>
-    
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="index.html">
-              <i class="mdi mdi-home menu-icon"></i>
-              <span class="menu-title">INICIO</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-              <i class="mdi mdi-account-multiple menu-icon"></i>
-              <span class="menu-title"> Administrar Prestadores</span>
-              <i class="menu-arrow"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="http://localhost:81/catalogos">
-              <i class="mdi mdi-contacts menu-icon"></i>
-              <span class="menu-title">Catálogos</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="http://localhost:81/gendeof">
-              <i class="mdi mdi-file-document menu-icon"></i>
-              <span class="menu-title">Generación de Oficios</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="http://localhost:81/horas">
-              <i class="mdi mdi-calendar-clock menu-icon"></i>
-              <span class="menu-title"> Fechas y Horas</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="http://localhost:81/reportes">
-              <i class="mdi mdi mdi-folder menu-icon"></i>
-              <span class="menu-title">Reportes</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="http://localhost:81/aupago">
-              <i class="mdi mdi-file-document menu-icon"></i>
-              <span class="menu-title">Autorización de pago</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="http://localhost:81/gendecre">
-              <i class="mdi mdi-table-large menu-icon"></i>
-              <span class="menu-title">Generación de credencial</span>
-            </a>
-          </li>
-          
-      </nav>
-      <div class="container-fluid page-body-wrapper">
-        <div id="theme-settings" class="settings-panel">
-          <i class="settings-close mdi mdi-close"></i>
-          <p class="settings-heading">SIDEBAR SKINS</p>
-          <div class="sidebar-bg-options selected" id="sidebar-default-theme">
-            <div class="img-ss rounded-circle bg-light border mr-3"></div> Default
-          </div>
-          <div class="sidebar-bg-options" id="sidebar-dark-theme">
-            <div class="img-ss rounded-circle bg-dark border mr-3"></div> Dark
-          </div>
-          <p class="settings-heading mt-2">HEADER SKINS</p>
-          <div class="color-tiles mx-0 px-4">
-            <div class="tiles light"></div>
-            <div class="tiles dark"></div>
-          </div>
-        </div>
-        <nav class="navbar col-lg-12 col-12 p-lg-0 fixed-top d-flex flex-row">
-          <div class="navbar-menu-wrapper d-flex align-items-stretch justify-content-between">
-            <a class="navbar-brand brand-logo-mini align-self-center d-lg-none" href="index.html"><img src="assets/images/logo-mini.svg" alt="logo" /></a>
-            <button class="navbar-toggler navbar-toggler align-self-center mr-2" type="button" data-toggle="minimize">
-              <i class="mdi mdi-menu"></i>
-            </button>
-            <ul class="navbar-nav">
-              <li class="nav-item dropdown">
-                
-              </li>
-            
-            </ul>
-            <ul class="navbar-nav navbar-nav-right ml-lg-auto">
-              <li class="nav-item dropdown d-none d-xl-flex border-0">
-              </li>
-            <li class="nav-item nav-profile dropdown border-0">
-                <a class="nav-link dropdown-toggle" id="profileDropdown" href="#" data-toggle="dropdown">
-                  <img class="nav-profile-img mr-2" alt="" src="assets/images/faces/face1.jpg" />
-                  <span class="profile-name">ADMINISTRADOR</span>
-                </a>
-                <div class="dropdown-menu navbar-dropdown w-100" aria-labelledby="profileDropdown">
-                  <a class="dropdown-item" href="#">
-                    <i class="mdi mdi-logout mr-2 text-primary"></i> cerrar sesión </a>
-                </div>
-            </li>
-            </ul>
-            <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
-              <span class="mdi mdi-menu"></span>
-            </button>
-          </div>
-        </nav>
-        <div class="main-panel">
-          <div class="content-wrapper pb-0">
-            <div class="page-header flex-wrap">
-              <h3 class="text-center" style="padding-top: 15px;"> SERVICIO SOCIAL</h3>
-            </div>
-                    
-            <div class="row">
-              <div class="col-xl-4 grid-margin stretch-card">
-                <div class="card">
-                  <div class="card-body">
-                    <h4 class="card-title text-black">Lista de Tareas Pendientes</h4>
-                    <p class="text-muted">Crear</p>
-                    <div class="list-wrapper">
-                      <ul class="d-flex flex-column-reverse todo-list todo-list-custom">
-                        <li>
-                          <div class="form-check">
-                            <label class="form-check-label">
-                              <input class="checkbox" type="checkbox" /> Meeting with Alisa </label>
-                            <span class="list-time">4 Hours Ago</span>
-                          </div>
-                        </li>
-                        <li>
-                          <div class="form-check">
-                            <label class="form-check-label">
-                              <input class="checkbox" type="checkbox" /> Create invoice </label>
-                            <span class="list-time">6 Hours Ago</span>
-                          </div>
-                        </li>
-                        <li class="completed">
-                          <div class="form-check">
-                            <label class="form-check-label">
-                              <input class="checkbox" type="checkbox" checked /> Prepare for presentation </label>
-                            <span class="list-time">2 Hours Ago</span>
-                          </div>
-                        </li>
-                        <li>
-                          <div class="form-check">
-                            <label class="form-check-label">
-                              <input class="checkbox" type="checkbox" /> Pick up kids from school </label>
-                            <span class="list-time">8 Hours Ago</span>
-                          </div>
-                        </li>
-                      </ul>
-                    </div>
-                    <div class="add-items d-flex flex-wrap flex-sm-nowrap">
-                      <input type="text" class="form-control todo-list-input flex-grow" placeholder="Add task name" />
-                      <button class="add btn btn-primary font-weight-regular text-nowrap" id="add-task"> Add Task </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-xl-4 col-md-6 grid-margin stretch-card">
-                <!--datepicker-->
-                <div class="card">
-                  <div class="card-body">
-                    <div id="inline-datepicker" class="datepicker table-responsive"></div>
-                  </div>
-                </div>
-
-        <!-- main-panel ends -->
-      </div>
-      <!-- page-body-wrapper ends -->
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+        <div style="display: flex; justify-content: center;">
+        
     </div>
-    <!-- container-scroller -->
-    <!-- plugins:js -->
-    <script src="assets/vendors/js/vendor.bundle.base.js"></script>
-    <!-- endinject -->
-    <!-- Plugin js for this page -->
-    <script src="assets/vendors/chart.js/Chart.min.js"></script>
-    <script src="assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
-    <script src="assets/vendors/flot/jquery.flot.js"></script>
-    <script src="assets/vendors/flot/jquery.flot.resize.js"></script>
-    <script src="assets/vendors/flot/jquery.flot.categories.js"></script>
-    <script src="assets/vendors/flot/jquery.flot.fillbetween.js"></script>
-    <script src="assets/vendors/flot/jquery.flot.stack.js"></script>
-    <script src="assets/vendors/flot/jquery.flot.pie.js"></script>
-    <!-- End plugin js for this page -->
-    <!-- inject:js -->
-    <script src="assets/js/off-canvas.js"></script>
-    <script src="assets/js/hoverable-collapse.js"></script>
-    <script src="assets/js/misc.js"></script>
-    <!-- endinject -->
-    <!-- Custom js for this page -->
-    <script src="assets/js/dashboard.js"></script>
-    <!-- End custom js for this page -->
-  </body>
-</html>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                    <body>
+                </div>
+            </div>
+        </div>
+    </div>
+  
+
+</x-app-layout>
