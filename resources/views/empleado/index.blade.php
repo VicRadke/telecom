7b21bb7809ce642f5d1ac951dd22fc70e8525a6d
Victor Mendoza <willradke1@gmail.com>
2023-12-20 10:26:06 -0600

    quitar js de form

--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -21,18 +21,6 @@
             <h1 class="text-green font-weight-bold text-uppercase mt-2 mb-3">Administrar prestadores</h1>
 
             <div class="contendor-accesos-directos">
-                <div class="accesos-directos pt-3" onclick="mostrarFormulario('div1')">
-                    <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
-                            class="bi bi-person-check" viewBox="0 0 16 16">
-                            <path
-                                d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514ZM11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
-                            <path
-                                d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z" />
-                        </svg>
-                        <p>Crear Solicitud</p>
-                    </div>
-                </div>
                 <div class="accesos-directos pt-3" onclick="mostrarFormulario('div2')">
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
@@ -70,7 +58,7 @@
             </div>
 
 
-            <div class="column" id="div1" style="display: none;">
+            <div">
                 <h1 class="text-green font-weight-bold text-uppercase">Solicitud de Servicio Social y Prácticas
                     Profesionales</h1>
                 <form class="form" method='POST' action="{{route('solicitud.store')}}">
@@ -732,7 +720,7 @@
                                 clip-rule="evenodd" fill-rule="evenodd"></path>
                         </g>
                     </svg>
-                </div>
+                
             </div>
 
             <div class="column" id="div4" style="display: none;">
